238e5f9598d78732bc2501c33da49ecfd916e101
cshashank <chitnis.shashank@gmail.com>
2019-11-03 20:45:36 -0500

    Completed facilties script

---
+++ scripts1/draImportFacilities.js
@@ -0,0 +1,9 @@
+WITH "file:///dra_node_facility.csv"
+AS uri
+LOAD CSV WITH HEADERS FROM uri  AS row
+MERGE (facitlity:Facility {id:row.id})
+SET facility.name = row.facility_name,
+    facility.lat = row.lat,
+    facility.long = row.long,
+    facility.capacity = row.capacity,
+    facility.processingTasks = row.processingTasks
