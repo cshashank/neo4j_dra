04b116d3f5a7ba2a15171a4bd670f3c3b88413b8
cshashank <chitnis.shashank@gmail.com>
2019-11-06 00:02:53 -0500

    loaded facilities and cost

---
+++ scripts1/importDRARelationshipFacilityToFacility.js
@@ -0,0 +1,6 @@
+WITH "file:///dra_relationship_facility_to_facility.csv"
+AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH (source:Facility {id: row.src})
+MATCH (destination:Facility {id: row.dest})
+MERGE (source)-[:COST]->(destination)
