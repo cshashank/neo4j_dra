c3e44a8bb20cc474943d959a4e8caf06c48663c3
cshashank <chitnis.shashank@gmail.com>
2019-11-07 08:18:16 -0500

    working on shortest path algo

---
+++ scripts1/importAllDRA.js
@@ -0,0 +1,26 @@
+match (n) detach delete n
+WITH "file:///drpw_clients_node.csv"
+AS uri
+LOAD CSV WITH HEADERS FROM uri  AS row
+MERGE(client: Client { id: row.id })
+SET client.firstName = row.frist_name,
+    client.lasttName = row.last_name
+
+
+WITH "file:///dra_node_facility.csv"
+AS uri
+LOAD CSV WITH HEADERS FROM uri  AS row
+MERGE(facility: Facility { id: row.id })
+SET facility.name = row.facility_name,
+    facility.lat = row.lat,
+    facility.long = row.long,
+    facility.capacity = row.capacity,
+    facility.processingTasks = row.processingTasks
+
+WITH "file:///dra_relationship_facility_to_facility.csv"
+AS uri
+LOAD CSV WITH HEADERS FROM uri AS row
+MATCH(source: Facility { id: row.src })
+MATCH(destination: Facility { id: row.dest })
+MERGE(source) - [: COST { cost: toInteger(row.cost) }] -> (destination)
+
--- scripts1/importDRARelationshipFacilityToFacility.js
+++ scripts1/importDRARelationshipFacilityToFacility.js
@@ -3,4 +3,4 @@ AS uri
 LOAD CSV WITH HEADERS FROM uri AS row
 MATCH (source:Facility {id: row.src})
 MATCH (destination:Facility {id: row.dest})
-MERGE (source)-[:COST]->(destination)
+MERGE (source)-[:COST {cost: toInteger(row.cost)}]->(destination)
